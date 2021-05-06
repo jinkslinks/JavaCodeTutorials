d810a460f5f43ea447a323238d29d286d8906b3d
jaifarmer99@gmail.com <s3762742@student.rmit.edu.au>
2021-05-06 16:38:35 +1000

    TestSort application on fruits

---
+++ weekContent/src/week9.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class week9 {
+    public static void main(String[] args) {
+        //array with fruits
+        String[] s = {"Oranges", "Apples", "Pears", "Bananas"};
+        ArrayList<String> words = new ArrayList<String>();
+        //adding fruits to words arraylist
+        Collections.addAll(words, s);
+
+        // this sorts the values by aplhabetical order
+        Collections.sort(words, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                return s1.compareTo(s2);
+            }
+        });
+
+        System.out.println(words);
+        //This then sorts the array by size of values
+        Collections.sort(words, (s1, s2) -> s1.length() - s2.length());
+        System.out.println(words);
+
+    }
+}
